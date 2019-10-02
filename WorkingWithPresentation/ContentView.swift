a3838dde9234416f94df5e7ad85a472374b6efd7
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-02 17:09:06 +0300

    How to show an alert

--- WorkingWithPresentation/ContentView.swift
+++ WorkingWithPresentation/ContentView.swift
@@ -9,8 +9,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+
     var body: some View {
-        Text("Hello World")
+        Button(action: {
+            self.showingAlert = true
+        }) {
+            Text("Show Alert")
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Important message"), message: Text("Wear sunscreen"), dismissButton: .default(Text("Got it!")))
+        }
     }
 }
 
